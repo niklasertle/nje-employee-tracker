dd20df448555f6c3b5e58a6ec03c8ba9164cca1e
Niklas Ertle <hockeytertle@gmail.com>
2021-09-13 14:57:12 -0600

    View roles and employee now show names of departments and roles instead of id numbers. Added functionality to be able to get all employees by manager

--- server.js
+++ server.js
@@ -88,35 +88,69 @@ function init() {
     });
 };
 
+
+// ============================== View Option Functions ==============================
 // Function to handle when the user selects a view option
 function viewCalls(res) {
     if (res.userChoice === "View all departments") {
+        // Returns all departments and IDs
         db.query('SELECT * FROM department', (err, data) => {
             if (err) throw err
             console.table(data)
             init()
         })
     } else if (res.userChoice === "View all roles") {
+        // Returns all roles with department associated with and IDs
         //TODO: RETURN dapartments instead of id#
-        db.query('SELECT * FROM roles', (err, data) => {
+        db.query('SELECT roles.id, roles.title, roles.salary, department.department_name FROM roles, department WHERE department.id = roles.department_id ORDER BY roles.id ASC', (err, data) => {
             if (err) throw err
             console.table(data)
             init()
         })
     } else if (res.userChoice === "View all employees") {
-        //TODO: RETURN names for roles and managers instead of a id#
-        db.query('SELECT * FROM employee', (err, data) => {
+        // Returns employee fist and last name, role title, and department name and displays in a table
+        db.query('SELECT employee.id, employee.first_name, employee.last_name, roles.title, department.department_name FROM employee, roles, department WHERE department.id = roles.department_id AND roles.id = employee.role_id ORDER BY employee.id ASC', (err, data) => {
+            if (err) throw err
             console.table(data)
+            init()
         })
     } else if (res.userChoice === "View all employees by manager") {
-        
+        viewEmpByManager()
     } else if (res.userChoice === "View all employees by department") {
         
     } else if (res.userChoice === "View total salary of a department") {
         
     }
-
 };
+
+// Gets all employees from a specific manager
+function viewEmpByManager() {
+    db.query("SELECT employee.id, employee.first_name ,employee.last_name FROM employee WHERE employee.manager_id IS NULL", (err, data) => {
+        if (err) throw err
+        let mngrArry = [];
+        data.forEach(element => mngrArry.push(`${element.first_name} ${element.last_name}`))
+        inquirer.prompt({
+            type: "list",
+            message: "Which manager do you want employees for?",
+            choices: mngrArry,
+            name: "manager"
+        }).then((res) => {
+            let managerId
+
+            data.forEach(element => {
+                if (res.manager === `${element.first_name} ${element.last_name}`) {
+                    managerId = element.id
+                }
+            })
+
+            db.query('SELECT * FROM employee WHERE manager_id = ?', managerId, (err, data) => {
+                if (err) throw err
+                console.table(data)
+                init()
+            })
+        })
+    })
+}
 
 // Calls initializer functions
 init();
