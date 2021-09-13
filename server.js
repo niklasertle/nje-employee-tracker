6a3208cfda99257c6ff5378f55ef64dc9d09110c
Niklas Ertle <hockeytertle@gmail.com>
2021-09-13 11:31:48 -0600

    Added initializer function to prompt using inquirer package. Removed express dependacy as it is not needed

--- server.js
+++ server.js
@@ -1,22 +1,90 @@
-const express = require('express');
-const inquirer = require('inquirer');
-const mysql2 = require('mysql2');
-const ct = require('console.table');
-require('dotenv').config();
-
-const PORT = process.env.PORT || 3001;
-const app = express();
-
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+const inquirer = require("inquirer");
+const mysql2 = require("mysql2");
+const ct = require("console.table");
+require("dotenv").config();
 
 const db = mysql2.createConnection(
-    process.env.JAWSDB_URL || 
-    {
-        host: 'localhost', 
-        user: 'root', 
-        password: process.env.sqlPass, 
-        database: 'employee_db'
-    },
-    console.log(`Connected to the employee database.`)
+  process.env.JAWSDB_URL || {
+    host: "localhost",
+    user: "root",
+    password: process.env.sqlPass,
+    database: "employee_db",
+  },
+  console.log(`Connected to the employee database.`)
 );
+
+let initialOptions = ["View", "Add", "Update", "Delete"];
+
+let viewOptions = [
+  "View all departments",
+  "View all roles",
+  "View all employees",
+  "View all employees by manager",
+  "View all employees by department",
+  "View total salary of a department",
+];
+
+let addOptions = ["Add a department", "Add a role", "Add employee"];
+
+let updateOptions = ["Update an employee role", "Update an employee manager"];
+
+let deleteOptions = ["Delete a department", "Delete a role", "Delete an employee"];
+
+function init() {
+  inquirer
+    .prompt({
+      type: "list",
+      message: "What do you wish to do?",
+      choices: initialOptions,
+      name: "userChoice",
+    })
+    .then((res) => {
+      if (res.userChoice === "View") {
+        inquirer
+          .prompt({
+            type: "list",
+            message: "What do you wish to view?",
+            choices: viewOptions,
+            name: "userChoice",
+          })
+          .then((res) => {
+            console.log(res);
+          });
+      } else if (res.userChoice === "Add") {
+        inquirer
+          .prompt({
+            type: "list",
+            message: "What do you wish to add?",
+            choices: addOptions,
+            name: "userChoice",
+          })
+          .then((res) => {
+            console.log(res);
+          });
+      } else if (res.userChoice === "Update") {
+        inquirer
+          .prompt({
+            type: "list",
+            message: "What do you wish to update?",
+            choices: updateOptions,
+            name: "userChoice",
+          })
+          .then((res) => {
+            console.log(res);
+          });
+      } else {
+        inquirer
+          .prompt({
+            type: "list",
+            message: "What do you wish to delete?",
+            choices: deleteOptions,
+            name: "userChoice",
+          })
+          .then((res) => {
+            console.log(res);
+          });
+      }
+    });
+}
+
+init();
